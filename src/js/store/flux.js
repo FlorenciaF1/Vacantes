27f5b60541c3b1c361e32f30853b874c68c86c92
Florencia Fernandez <96849628+FlorenciaF1@users.noreply.github.com>
2024-10-16 21:03:19 -0300

    Update the fetch url

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -17,7 +17,7 @@ const getState = ({ getStore, getActions, setStore }) => {
             password: password,
           };
 
-          const response = await fetch("https://vacantes-s4ce.onrender.com/admin/user", {
+          const response = await fetch("https://vacantes-s4ce.onrender.com/admin/users", {
             method: "POST",
             headers: {
               "Content-Type": "application/json",
